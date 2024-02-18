f236e5ab0061decb06c2a776eba3b97e61b20f49
richie007dev <richie@ardhika.com>
2024-02-19 00:52:04 +0530

    Add id in place model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,4 +1,9 @@
-class Place{
-  const Place({required this.title});
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Place {
+  Place({required this.title}) : id = uuid.v4();
+  final String id;
   final String title;
 }
