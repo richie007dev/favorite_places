635deed5f6293a070b9757ee8efe852fa14ba61d
richie007dev <richie@ardhika.com>
2024-02-18 19:04:01 +0530

    Modify favorite_place_provider.dart

--- lib/providers/favorite_place_provider.dart
+++ lib/providers/favorite_place_provider.dart
@@ -1,6 +1,14 @@
-import 'package:favorite_places/data/dummy_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:favorite_places/models/favorite_place.dart';
 
+class FavoritePlaceProvider extends StateNotifier<List<FavoritePlace>> {
+  FavoritePlaceProvider() : super([]);
+  void addPlace(FavoritePlace place) {
+    state = [...state, place];
+  }
+}
 
-
-final favoritePlaceProvider = Provider((ref) => dummyPlaces);
+final favoritePlaceProvider =
+    StateNotifierProvider<FavoritePlaceProvider, List<FavoritePlace>>(
+  (ref) => FavoritePlaceProvider(),
+);
