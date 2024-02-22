990a58eaa94ef38dbbf8f9fc7ca691ba86851d2d
richie007dev <richie@ardhika.com>
2024-02-23 01:31:00 +0530

    Change places.dart into ConsumerStateful Widget. Using FutureBuilder widget for place list.

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -5,10 +5,24 @@ import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> _placesFuture;
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final List<Place> favoritePlaces = ref.watch(userPlacesProvider);
     return Scaffold(
       appBar: AppBar(
@@ -26,8 +40,16 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: favoritePlaces,
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : PlacesList(
+                      places: favoritePlaces,
+                    ),
         ),
       ),
     );
